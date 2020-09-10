fec5f3606c7e4559b3840ee8b0431e208aaa7434
Ivanskiy-Maxim <61918520+Ivanskiy-Maxim@users.noreply.github.com>
2020-09-10 21:49:47 +0400

    //

---
+++ home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pavlo</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!--    fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+
+    <!--    fonts-->
+</head>
+
+<body>
+    <div class="header">
+        <div class="header_phone_number">
+            <div class="header_phone_number_1_div"> <img src="img/icons8-viber-35.png" alt="">
+              
+              <p class="header_phone_number_1">+380673232028</p>
+               
+                
+            </div>
+            <div class="header_phone_number_2_div"> <img src="img/icons8-viber-35.png" alt="">
+                
+                <p class="header_phone_number_1">+380673232028</p>
+                
+            </div>
+        </div>
+        <div class="header_navigation">
+            <div class="div_icon">
+                <a href=""><img src="img/spotify.png" alt=""></a>
+            </div>
+            <div class="div_nav">
+                <ul>
+                    <li><a href="#">Ворота</a></li>
+                    <li><a href="#">Ворота</a></li>
+                    <li><a href="#">Ворота</a></li>
+                    <li><a href="#">Ворота</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    
+    <div class="main">
+<!--       <div class="free"></div>-->
+        <img src="img/spotify.png" alt="">
+        
+    </div>
+    
+    
+    
+    
+    <div class="footer"></div>
+</body>
+
+</html>
